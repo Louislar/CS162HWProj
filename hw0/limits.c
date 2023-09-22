aa986e8307a789d5334eca10fd09bcfd4cfcf5a2
louisLar <r09922a02@cmlab.csie.ntu.edu.tw>
2023-09-22 13:11:52 +0000

    [hw0] Finished 3.2 user limits Successfully print three limits, including stack size, number of process, and number of file descriptor.

--- hw0/limits.c
+++ hw0/limits.c
@@ -2,9 +2,21 @@
 #include <sys/resource.h>
 
 int main() {
-    struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    struct rlimit stackLim;
+    struct rlimit procLim;
+    struct rlimit fileLim;
+    if(
+        getrlimit(RLIMIT_STACK, &stackLim) == 0 && 
+        getrlimit(RLIMIT_NPROC, &procLim) == 0 &&
+        getrlimit(RLIMIT_NOFILE, &fileLim) == 0
+    ) {
+        printf("stack size: %ld\n", stackLim.rlim_cur);
+        printf("process limit: %ld\n", procLim.rlim_cur);
+        printf("max file descriptors: %ld\n", fileLim.rlim_cur);
+    }
+    else {
+        printf("Somthing went wrong with syscall \"getrlimit\" \n");
+    }
+        
     return 0;
 }
